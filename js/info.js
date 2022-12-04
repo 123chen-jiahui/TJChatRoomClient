be136a3b70078d35b7e5a6cff152ae32c7c8f52f
chen <1748567982@qq.com>
2022-12-04 19:17:17 +0800

    群聊界面、伪头像

--- js/info.js
+++ js/info.js
@@ -4,3 +4,20 @@ var friends = [] // 获取的好友信息
 var groups = []
 
 var messages_unread_raw = []
+
+// 根据account返回nickName
+function getNickName(account) {
+  for (var i = 0; i < friends.length; i ++) {
+    if (friends[i].account == account) {
+      return friends[i].nickName
+    }
+  }
+}
+
+function getGroupName(groupId) {
+  for (var i = 0; i < groups.length; i ++) {
+    if (groups[i].Id == groupId) {
+      return groups[i].Name
+    }
+  }
+}
