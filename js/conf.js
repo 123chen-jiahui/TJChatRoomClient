b9f05e237d717e87b1251afc0d5e34116c1d09e9
chen <1748567982@qq.com>
2022-12-05 11:26:12 +0800

    同步

--- js/conf.js
+++ js/conf.js
@@ -1,1 +1,2 @@
 const BaseUrl = 'http://localhost:8888/'
+const AvatarUrlPrefix = 'https://tj-chat-room.oss-cn-shanghai.aliyuncs.com/avatar/'
--- js/info.js
+++ js/info.js
@@ -1,9 +1,11 @@
 var me = {} // 自身的信息
 var friends = [] // 获取的好友信息
 
-var groups = []
+var groups = [] // 获取的群聊信息
 
-var messages_unread_raw = []
+var messages_unread_raw = [] // 登录后获取的message
+
+const Contacts = new Map() // 通信录，找不到的人都放这
 
 // 根据account返回nickName
 function getNickName(account) {
@@ -12,6 +14,15 @@ function getNickName(account) {
       return friends[i].nickName
     }
   }
+}
+
+function findFriend(account) {
+  for (var i = 0; i < friends.length; i ++) {
+    if (friends[i].account == account) {
+      return friends[i]
+    }
+  }
+  return {}
 }
 
 function getGroupName(groupId) {
