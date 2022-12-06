fb7d82db2b96dbf7263505d25c0085abdd161bd0
chen <1748567982@qq.com>
2022-12-06 23:57:23 +0800

    支持文件信息

--- js/conf.js
+++ js/conf.js
@@ -1,2 +1,3 @@
 const BaseUrl = 'http://localhost:8888/'
 const AvatarUrlPrefix = 'https://tj-chat-room.oss-cn-shanghai.aliyuncs.com/avatar/'
+const OssPrefix = 'https://tj-chat-room.oss-cn-shanghai.aliyuncs.com/files/'
