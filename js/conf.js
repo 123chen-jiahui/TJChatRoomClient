29f7fd8f7f4572f5b461332d7e49b6dbc16de7bc
chen <1748567982@qq.com>
2022-12-20 16:14:38 +0800

    update

--- js/conf.js
+++ js/conf.js
@@ -1,3 +1,4 @@
-const BaseUrl = 'http://localhost:8888/'
+// const BaseUrl = 'http://localhost:8888/'
+const BaseUrl = 'http://118.31.108.144:8888/'
 const AvatarUrlPrefix = 'https://tj-chat-room.oss-cn-shanghai.aliyuncs.com/avatar/'
 const OssPrefix = 'https://tj-chat-room.oss-cn-shanghai.aliyuncs.com/files/'
